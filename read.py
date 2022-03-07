c355f030491c49e211b3b7bd46e5852e06a39de8
Smaran Chand <smaranchand@gmail.com>
2022-03-07 17:09:22 +0545

    Added warning and stuffs

--- read.py
+++ read.py
@@ -2,6 +2,8 @@
 from contextlib import nullcontext
 import re
 import sys
+import select
+import sys
 #from turtle import done
 import pymongo
 print('''
@@ -10,22 +12,29 @@ print('''
 ██ ██  ██ ██    ██ ██      █████ ██   ███ ██    ██ ██
 ██  ██ ██ ██    ██ ██            ██    ██ ██    ██ ██
 ██   ████  ██████   ██████        ██████   ██████  ██
+
+Powered by Daal Bhaat
 ''')
-myclient = pymongo.MongoClient('mongodb+srv://root:localhost123@nuc-gui-db.zolos.mongodb.net/scanresults?retryWrites=true&w=majority')
-rawdata = sys.stdin.read()
-regexmatch = re.compile(r'\x1b[^m]*m')
-results = regexmatch.sub('', rawdata)
-new=(results.translate(results.maketrans({'[': '', ']': ''})))
-for data in new.splitlines():
-    arr=data.split()
-    date=(arr[0])
-    time=(arr[1])
-    vulnerability=(arr[2])
-    scope=(arr[3])
-    severity=(arr[4])
-    endpoint=(arr[5])
-    mydb = myclient["scanresults"]
-    mycol = mydb["nuclei_results"]
-    data = { "date":""+date , "time":""+time , "vulnerability":""+vulnerability , "scope":""+scope , "severity":""+severity , "endpoint":""+endpoint }
-    x = mycol.insert_one(data)
-    print("[+] Results migrated to database.[+]")
+
+if select.select([sys.stdin, ], [], [], 0.0)[0]:
+
+    myclient = pymongo.MongoClient('mongodb+srv://root:localhost123@nuc-gui-db.zolos.mongodb.net/scanresults?retryWrites=true&w=majority')
+    rawdata = sys.stdin.read()
+    regexmatch = re.compile(r'\x1b[^m]*m')
+    results = regexmatch.sub('', rawdata)
+    new=(results.translate(results.maketrans({'[': '', ']': ''})))
+    for data in new.splitlines():
+        arr=data.split()
+        date=(arr[0])
+        time=(arr[1])
+        vulnerability=(arr[2])
+        scope=(arr[3])
+        severity=(arr[4])
+        endpoint=(arr[5])
+        mydb = myclient["scanresults"]
+        mycol = mydb["nuclei_results"]
+        data = { "date":""+date , "time":""+time , "vulnerability":""+vulnerability , "scope":""+scope , "severity":""+severity , "endpoint":""+endpoint }
+        x = mycol.insert_one(data)
+        print("[+] Results migrated to database.[+]")
+else:
+    print("No output data detected.")
