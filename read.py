889f4fbbf89ffb76c0054181e285c728acf63c79
yunish_stha <32809342+ysthas@users.noreply.github.com>
2022-05-31 23:39:58 +0545

    added yaml parser to fetch configuration
    Signed-off-by: yunish_stha <32809342+ysthas@users.noreply.github.com>

--- read.py
+++ read.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
 import re
 import sys
-from turtle import done
+
 import pymongo
-myclient = pymongo.MongoClient('mongodb+srv://root:localhost123@nuc-gui-db.zolos.mongodb.net/scanresults?retryWrites=true&w=majority')
+import yaml
+
+with open("config.yaml", "r") as yamlfile:
+    config_data = yaml.load(yamlfile, Loader=yaml.FullLoader)
+
+myclient = pymongo.MongoClient(config_data['Config']['MONGO_URI'])
 rawdata = sys.stdin.read()
 regexmatch = re.compile(r'\x1b[^m]*m')
 results = regexmatch.sub('', rawdata)
-new=(results.translate(results.maketrans({'[': '', ']': ''})))
+new = (results.translate(results.maketrans({'[': '', ']': ''})))
 for data in new.splitlines():
-    arr=data.split()
-    date=(arr[0])
-    time=(arr[1])
-    vulnerability=(arr[2])
-    scope=(arr[3])
-    severity=(arr[4])
-    endpoint=(arr[5])
+    arr = data.split()
+    date = (arr[0])
+    time = (arr[1])
+    vulnerability = (arr[2])
+    scope = (arr[3])
+    severity = (arr[4])
+    endpoint = (arr[5])
     mydb = myclient["scanresults"]
     mycol = mydb["nuclei_results"]
-    data = { "date":""+date , "time":""+time , "vulnerability":""+vulnerability , "scope":""+scope , "severity":""+severity , "endpoint":""+endpoint }
+    data = {"date": "" + date, "time": "" + time, "vulnerability": "" + vulnerability, "scope": "" + scope,
+            "severity": "" + severity, "endpoint": "" + endpoint}
     x = mycol.insert_one(data)
     print("Results migrated to database.")
