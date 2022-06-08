cc61fc0b10b54b6a123e3b04856f22f3bb87b835
yunish_stha <32809342+ysthas@users.noreply.github.com>
2022-06-08 16:00:05 +0545

    Added Database_name in yaml and Cleaning unused resources and code.
    Signed-off-by: yunish_stha <32809342+ysthas@users.noreply.github.com>

--- read.py
+++ read.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-#from contextlib import nullcontext
+# from contextlib import nullcontext
 import re
-import sys
 import select
-import yaml
-#from turtle import done
+import sys
+
+# from turtle import done
 import pymongo
+import yaml
+
 print('''
 ███    ██ ██    ██  ██████        ██████  ██    ██ ██
 ████   ██ ██    ██ ██            ██       ██    ██ ██
@@ -15,27 +17,30 @@ print('''
 
 Developed by:
 ''')
-
-if select.select([sys.stdin, ], [], [], 0.0)[0]:
+try:
     with open("config.yaml", "r") as yamlfile:
         config_data = yaml.load(yamlfile, Loader=yaml.FullLoader)
-
     myclient = pymongo.MongoClient(config_data['Config']['MONGO_URI'])
+except Exception as e:
+    print(e)
+if select.select([sys.stdin, ], [], [], 0.0)[0]:
+
     rawdata = sys.stdin.read()
     regexmatch = re.compile(r'\x1b[^m]*m')
     results = regexmatch.sub('', rawdata)
-    new=(results.translate(results.maketrans({'[': '', ']': ''})))
+    new = (results.translate(results.maketrans({'[': '', ']': ''})))
     for data in new.splitlines():
-        arr=data.split()
-        date=(arr[0])
-        time=(arr[1])
-        vulnerability=(arr[2])
-        scope=(arr[3])
-        severity=(arr[4])
-        endpoint=(arr[5])
-        mydb = myclient["scanresults"]
+        arr = data.split()
+        date = (arr[0])
+        time = (arr[1])
+        vulnerability = (arr[2])
+        scope = (arr[3])
+        severity = (arr[4])
+        endpoint = (arr[5])
+        mydb = pymongo.MongoClient(config_data['Config']['DATABASE_NAME'])
         mycol = mydb["nuclei_results"]
-        data = { "date":""+date , "time":""+time , "vulnerability":""+vulnerability , "scope":""+scope , "severity":""+severity , "endpoint":""+endpoint }
+        data = {"date": "" + date, "time": "" + time, "vulnerability": "" + vulnerability, "scope": "" + scope,
+                "severity": "" + severity, "endpoint": "" + endpoint}
         x = mycol.insert_one(data)
         print("[+] Results migrated to database.[+]")
 else:
