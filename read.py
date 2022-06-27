39d5ad6bd683a63ea6f29144d119445ebdd18230
Smaran Chand <smaranchand@gmail.com>
2022-06-27 13:41:32 +0545

    stable version

--- read.py
+++ read.py
@@ -19,29 +19,24 @@ print('''
 
 Developed by: Smaran & Yunish
 ''')
-try:
-    with open("config.yaml", "r") as yamlfile:
-        config_data = yaml.load(yamlfile, Loader=yaml.FullLoader)
-except Exception as e:
-    print(e)
-if select.select([sys.stdin,], [], [], 0.0)[0]:
+if select.select([sys.stdin, ], [], [], 0.0)[0]:
+
+    myclient = pymongo.MongoClient('mongodb+srv://root:localhost123@nuc-gui-db.zolos.mongodb.net/scanresults?retryWrites=true&w=majority')
     rawdata = sys.stdin.read()
     regexmatch = re.compile(r'\x1b[^m]*m')
     results = regexmatch.sub('', rawdata)
-    new = (results.translate(results.maketrans({'[': '', ']': ''})))
+    new=(results.translate(results.maketrans({'[': '', ']': ''})))
     for data in new.splitlines():
-        arr = data.split()
-        date = (arr[0])
-        time = (arr[1])
-        vulnerability = (arr[2])
-        scope = (arr[3])
-        severity = (arr[4])
-        endpoint = (arr[5])
-        myclient = pymongo.MongoClient(config_data['Config']['MONGO_URI'])
-        print(myclient)
-        mydb = pymongo.MongoClient(config_data['Config']['DATABASE_NAME'])
+        arr=data.split()
+        date=(arr[0])
+        time=(arr[1])
+        vulnerability=(arr[2])
+        scope=(arr[3])
+        severity=(arr[4])
+        endpoint=(arr[5])
+        mydb = myclient["scanresults"]
         mycol = mydb["nuclei_results"]
-        data = {"date": "" + date, "time": "" + time, "vulnerability": "" + vulnerability, "scope": "" + scope,"severity": "" + severity, "endpoint": "" + endpoint}
+        data = { "date":""+date , "time":""+time , "vulnerability":""+vulnerability , "scope":""+scope , "severity":""+severity , "endpoint":""+endpoint }
         x = mycol.insert_one(data)
         print("[+] Results migrated to database.[+]")
 else:
