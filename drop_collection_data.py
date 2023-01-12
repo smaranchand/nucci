5e42e6d4d8e66205616eb8c7e3d0886f159a5cf8
Smaran Chand <Smaranchand@gmail.com>
2023-01-13 00:15:19 +0545

    Delete drop_collection_data.py

--- drop_collection_data.py
+++
@@ -1,5 +0,0 @@
-import pymongo
-myclient = pymongo.MongoClient("mongodb+srv://root:localhost123@nuc-gui-db.zolos.mongodb.net/scanresults?retryWrites=true&w=majority")
-mydb = myclient["scanresults"]
-mycol = mydb["nuclei_results"]
-mycol.drop()
